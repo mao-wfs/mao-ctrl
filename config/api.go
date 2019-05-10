7516ee0c305b1e1e73a420aef7da875597f66b41
scizorman <tueda1207@gmail.com>
2019-05-10 19:30:13 +0900

    [add] Config loader from environment variables

--- config/api.go
+++ config/api.go
@@ -2,12 +2,28 @@ package config
 
 import (
 	"fmt"
+
+	"golang.org/x/xerrors"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
 // APIConfig represents the conigure of the API.
 type APIConfig struct {
-	Host string `toml:"host"`
-	Port int    `toml:"port"`
+	// Host is the API host.
+	Host string
+
+	// Port is the port number the API listen on.
+	Port int `default:"3000"`
+}
+
+// LoadAPIConfig loads the API configuration from environment variables.
+func LoadAPIConfig() (*APIConfig, error) {
+	conf := new(APIConfig)
+	if err := envconfig.Process("api", conf); err != nil {
+		return nil, xerrors.Errorf("failed to load the API configuration: %w", err)
+	}
+	return conf, nil
 }
 
 // GetAddr returns the API address.
--- config/device.go
+++ config/device.go
@@ -2,6 +2,10 @@ package config
 
 import (
 	"fmt"
+
+	"golang.org/x/xerrors"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
 // DeviceConfig represents the configure of MAO-WFS devices.
@@ -22,8 +26,20 @@ func (c *DeviceConfig) GetFGConfig() FGConfig {
 
 // CorrelatorConfig represents the configure of the correlator.
 type CorrelatorConfig struct {
-	Host string `toml:"host"`
-	Port int    `toml:"port"`
+	// Host is the correlator host.
+	Host string `required:"true"`
+
+	// Port is the port number the correlator listen on.
+	Port int `required:"true"`
+}
+
+// LoadCorrelatorConfig loads the correlator configuration from environment variables.
+func LoadCorrelatorConfig() (*CorrelatorConfig, error) {
+	conf := new(CorrelatorConfig)
+	if err := envconfig.Process("correlator", conf); err != nil {
+		return nil, xerrors.Errorf("failed to load the correlator configuration: %w", err)
+	}
+	return conf, nil
 }
 
 // GetAddr returns the correlator address.
@@ -34,9 +50,23 @@ func (c *CorrelatorConfig) GetAddr() string {
 
 // FGConfig represents the configure of the FG.
 type FGConfig struct {
-	Host  string `toml:"host"`
-	Port  int    `toml:"port"`
-	Order []int  `toml:"switch-order"`
+	// Host is the FG host.
+	Host string `required:"true"`
+
+	// Port is the port number the FG listen on.
+	Port int `required:"true"`
+
+	// Order is the switching order.
+	Order []int `default:"10,9,13,8,0,80,16,32"`
+}
+
+// LoadFGConfig loads the FG configuration from environment variables.
+func LoadFGConfig() (*FGConfig, error) {
+	conf := new(FGConfig)
+	if err := envconfig.Process("fg", conf); err != nil {
+		return nil, xerrors.Errorf("failed to load the FG configuration: %w", err)
+	}
+	return conf, nil
 }
 
 // GetAddr returns the FG address.
