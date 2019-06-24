98cded352d754c2d05a78889932feaaba39c9fe3
scizorman <tueda1207@gmail.com>
2019-06-24 17:51:15 +0900

    [change] Rename

--- internal/app/interfaces/gateway/device/correlator/handler.go
+++ internal/app/interfaces/gateway/device/correlator/handler.go
@@ -1,11 +1,11 @@
-package device
+package correlator
 
 import (
 	"context"
 )
 
-// FGHandler is the interface that describe the FG handler.
-type FGHandler interface {
+// Handler is the interface that describe the correlator handler.
+type Handler interface {
 	Start(ctx context.Context) error
 	Halt(ctx context.Context) error
 	Initialize(ctx context.Context) error
--- internal/app/interfaces/gateway/device/fg/handler.go
+++ internal/app/interfaces/gateway/device/fg/handler.go
@@ -1,11 +1,11 @@
-package device
+package fg
 
 import (
 	"context"
 )
 
-// CorrelatorHandler is the interface that describe the correlator handler.
-type CorrelatorHandler interface {
+// Handler is the interface that describe the FG handler.
+type Handler interface {
 	Start(ctx context.Context) error
 	Halt(ctx context.Context) error
 	Initialize(ctx context.Context) error
