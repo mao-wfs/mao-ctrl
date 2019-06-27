759a1589bc79fdcbc29b934b0d0d69f54ba64f68
scizorman <tueda1207@gmail.com>
2019-06-27 15:29:52 +0900

    [add] Context timeout

--- internal/app/usecases/interactor/wfs.go
+++ internal/app/usecases/interactor/wfs.go
@@ -2,6 +2,7 @@ package interactor
 
 import (
 	"context"
+	"time"
 
 	"golang.org/x/xerrors"
 
@@ -9,6 +10,8 @@ import (
 	"github.com/mao-wfs/mao-ctrl/internal/app/usecases/input"
 	"github.com/mao-wfs/mao-ctrl/internal/app/usecases/output"
 )
+
+const contextTimeout = 10 * time.Second
 
 // WFSInteractor is the interactor of MAO-WFS services.
 type WFSInteractor struct {
@@ -28,12 +31,15 @@ func NewWFSInteractor(s *domain.Status, h domain.Handler, errOpt output.ErrorOut
 
 // Start starts MAO-WFS.
 func (i *WFSInteractor) Start(ctx context.Context) output.Error {
+	subCtx, cancel := context.WithTimeout(ctx, contextTimeout)
+	defer cancel()
+
 	if i.status.IsRunning() {
 		err := xerrors.Errorf("MAO-WFS is already running")
-		return i.errorOutputPort.BadRequest(ctx, err)
+		return i.errorOutputPort.BadRequest(subCtx, err)
 	}
-	if err := i.handler.Start(ctx); err != nil {
-		return i.errorOutputPort.InternalServerError(ctx, err)
+	if err := i.handler.Start(subCtx); err != nil {
+		return i.errorOutputPort.InternalServerError(subCtx, err)
 	}
 	i.status.SetRunning()
 	return nil
@@ -41,12 +47,15 @@ func (i *WFSInteractor) Start(ctx context.Context) output.Error {
 
 // Halt halts MAO-WFS.
 func (i *WFSInteractor) Halt(ctx context.Context) output.Error {
+	subCtx, cancel := context.WithTimeout(ctx, contextTimeout)
+	defer cancel()
+
 	if i.status.IsWaiting() {
 		err := xerrors.Errorf("MAO-WFS is not running")
-		return i.errorOutputPort.BadRequest(ctx, err)
+		return i.errorOutputPort.BadRequest(subCtx, err)
 	}
-	if err := i.handler.Halt(ctx); err != nil {
-		return i.errorOutputPort.InternalServerError(ctx, err)
+	if err := i.handler.Halt(subCtx); err != nil {
+		return i.errorOutputPort.InternalServerError(subCtx, err)
 	}
 	i.status.SetWaiting()
 	return nil
